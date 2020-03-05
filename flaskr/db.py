b92b3a078f95f314b5c508d424251935553434b0
A.Arimura <404ari@gmail.com>
2020-03-05 12:08:54 +0900

    by register db

---
+++ flaskr/db.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+    
+    with current_app.open_resource('schema.sql') as f:
+        # execute SQL command from schema.sql
+        db.executescript(f.read().decode('utf8'))
+    
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    '''Clear the existing data and create new tables.'''
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    # when app stops(after returning response), close_db is called.
+    app.teardown_appcontext(close_db)
+    # add "flask init-db" as a new command
+    app.cli.add_command(init_db_command)
